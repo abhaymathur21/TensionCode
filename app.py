4cdab1c39f5cfe1889f8b4cae275b472dfc22b91
Abhay Mathur <a21.mathur21@gmail.com>
2024-02-18 06:59:32 +0530

    added receiving image feature for ocr

--- app.py
+++ app.py
@@ -5,9 +5,45 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.prompts import PromptTemplate
 import re
 from flask_cors import CORS
+from pathlib import Path
+import google.generativeai as genai
+
 
 if "GOOGLE_API_KEY" not in os.environ:
     os.environ["GOOGLE_API_KEY"] = "AIzaSyDbzDwrQ3gi3kM-gA8XpmdXQWpRDG_xtEc"
+    
+genai.configure(api_key="AIzaSyB7fSw9N5yT6Rhhz1y6HMUC_bsjGp2YwkQ")
+
+# Set up the model
+generation_config = {
+  "temperature": 0.4,
+  "top_p": 1,
+  "top_k": 32,
+  "max_output_tokens": 4096,
+}
+
+safety_settings = [
+  {
+    "category": "HARM_CATEGORY_HARASSMENT",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+  {
+    "category": "HARM_CATEGORY_HATE_SPEECH",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+  {
+    "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+  {
+    "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+]
+
+vision = genai.GenerativeModel(model_name="gemini-1.0-pro-vision-latest",
+                              generation_config=generation_config,
+                              safety_settings=safety_settings)
 
 llm = ChatGoogleGenerativeAI(model="gemini-pro")
 
@@ -35,6 +71,26 @@ async def generate_code():
         schema = None
         image = request.files.get("image", None)
         # Abhay here you can use the image to get the schema
+        
+        # # Validate that an image is present
+        # if not (img := Path("ER-Model.png")).exists():
+        #     raise FileNotFoundError(f"Could not find image: {img}")
+        
+        image_parts = [
+            {
+                "mime_type": "image/png",
+                "data": image.read()
+            },
+        ]
+        prompt_parts = [
+        image_parts[0],
+        "\n i have this ER diagram and i want to extract the schema in json text format, also give me the data types of the columns in the schema. Aslo i want to know the data types for each column in the schema. So for that we have to assume and classify accordingly, for example if a column is of type varchar then we have to assume it as string, if it is of type int then we have to assume it as integer, if it is of type date then we have to assume it as date, if it is of type time then we have to assume it as time, if it is of type datetime then we have to assume it as datetime, if it is of type timestamp then we have to assume it as timestamp, if it is of type year then we have to assume it as year, if it is of type text then we have to assume it as text, if it is of type longtext then we have to assume it as longtext, if it is of type mediumtext then we have to assume it as mediumtext, if it is of type tinytext then we have to assume it as tinytext.",
+        ]
+
+        response = vision.generate_content(prompt_parts)
+
+        print(response.text)
+        schema=response.text
 
     imput_data = {
         "language": language,
