68366dcb103092ae1daa33646e29b13b78fd7b8d
Abhay Mathur <a21.mathur21@gmail.com>
2024-02-18 07:43:54 +0530

    added schema handling for csv, json and xml files

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ import re
 from flask_cors import CORS
 from pathlib import Path
 import google.generativeai as genai
+import pandas as pd
+import xml.etree.ElementTree as ET
+
 
 
 if "GOOGLE_API_KEY" not in os.environ:
@@ -59,16 +62,17 @@ def index():
 @app.route("/generate_code", methods=["POST"])
 async def generate_code():
 
-    language = request.form["language"] if "language" in request.form else "python"
+    language = request.form.get("language", "python")
     function_template = request.form.get("function_template", "")
     input_params = request.form.get("input_params", "{}")
     output_format = request.form.get("output_format", "")
-    provider = request.form["provider"] if "provider" in request.form else "mongodb"
+    provider = request.form["provider"] if "provider" in request.form else ""
     schema = request.form.get("schema", "")
     task = request.form.get("task", "")
+    schema_format = request.form.get("schema_format", "")
 
-    if schema == "":
-        schema = None
+    if schema_format == "image":
+        
         image = request.files.get("image", None)
         # Abhay here you can use the image to get the schema
         
@@ -84,15 +88,39 @@ async def generate_code():
         ]
         prompt_parts = [
         image_parts[0],
-        "\n i have this ER diagram and i want to extract the schema in json text format, also give me the data types of the columns in the schema. Aslo i want to know the data types for each column in the schema. So for that we have to assume and classify accordingly, for example if a column is of type varchar then we have to assume it as string, if it is of type int then we have to assume it as integer, if it is of type date then we have to assume it as date, if it is of type time then we have to assume it as time, if it is of type datetime then we have to assume it as datetime, if it is of type timestamp then we have to assume it as timestamp, if it is of type year then we have to assume it as year, if it is of type text then we have to assume it as text, if it is of type longtext then we have to assume it as longtext, if it is of type mediumtext then we have to assume it as mediumtext, if it is of type tinytext then we have to assume it as tinytext.",
+        "\n i have this ER diagram and I want to extract the schema in json text format, also give me the data types of the columns in the schema. Also i want to know the data types for each column in the schema. So for that we have to assume and classify accordingly, for example if a column is of type varchar then we have to assume it as string, if it is of type int then we have to assume it as integer, if it is of type date then we have to assume it as date, if it is of type time then we have to assume it as time, if it is of type datetime then we have to assume it as datetime, if it is of type timestamp then we have to assume it as timestamp, if it is of type year then we have to assume it as year, if it is of type text then we have to assume it as text, if it is of type longtext then we have to assume it as longtext, if it is of type mediumtext then we have to assume it as mediumtext, if it is of type tinytext then we have to assume it as tinytext.",
         ]
 
         response = vision.generate_content(prompt_parts)
 
         print(response.text)
         schema=response.text
+            
+    elif schema_format == "table":
+        table = request.files.get("table", None)
+        filename = table.filename
+        if filename.endswith(".csv"):
+            df = pd.read_csv(table)
+        elif filename.endswith(".json"):
+            df = pd.read_json(table)
+        elif filename.endswith(".xml"):
+            # Read the XML file into a Pandas DataFrame
+            root = ET.parse(table).getroot()
+            data = []
+            for item in root:
+                data.append(item.attrib)
+            df = pd.DataFrame(data)
+            
+        prompt = f""" 
+        This is the data:\n{df.columns}\n{df.head()}
+        Given the above dataframe data, I want to extract the schema in json text format, also give me the data types of the columns in the schema. Also i want to know the data types for each column in the schema. So for that we have to assume and classify accordingly, for example if a column is of type varchar then we have to assume it as string, if it is of type int then we have to assume it as integer, if it is of type date then we have to assume it as date, if it is of type time then we have to assume it as time, if it is of type datetime then we have to assume it as datetime, if it is of type timestamp then we have to assume it as timestamp, if it is of type year then we have to assume it as year, if it is of type text then we have to assume it as text, if it is of type longtext then we have to assume it as longtext, if it is of type mediumtext then we have to assume it as mediumtext, if it is of type tinytext then we have to assume it as tinytext.
+        """
+        
+        result = llm.invoke(prompt)
+        print(response.content)
+        schema=response.content
 
-    imput_data = {
+    input_data = {
         "language": language,
         "function_template": function_template,
         "input_params": input_params,
@@ -103,7 +131,7 @@ async def generate_code():
     }
 
     prompt = [
-        """
+    """
     You are a code generation system.
     Your job is to generate a complete function that performs the mentioned task.
     The function input is parsed as an JSON object
@@ -153,7 +181,7 @@ async def generate_code():
 
     chain = template | llm
 
-    result = chain.invoke(imput_data)
+    result = chain.invoke(input_data)
     generated_code = result.content
     print(generated_code)
 
